62413501929cf605938ff436df47c2a912abc216
Alexy Sviridov <unskillton@gmail.com>
2025-02-07 08:31:31 +0300

    itertools

--- VS/PythonApplication2/PythonApplication2/PythonApplication2.py
+++ VS/PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,14 +1,13 @@
-'''
+import random
 import itertools
 
-accumulate_1 = [1,2,3,4,5]
-accumulate_2 = itertools.accumulate(accumulate_1)
-print(type(accumulate_2))
-'''
-
-import random
 
 some_list = [1,2,3,4,5]
 some_choice = random.choice(some_list)
 
-print(some_choice)
+#print(some_choice)
+
+
+accumulate_sum = list(itertools.accumulate(some_list))
+
+print(accumulate_sum)
