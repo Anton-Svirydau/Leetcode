069765baaab7a30ba53237eace78cdf3077a62b1
Alexy Sviridov <unskillton@gmail.com>
2025-02-07 08:26:37 +0300

    random

---
+++ VS/PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -0,0 +1,14 @@
+'''
+import itertools
+
+accumulate_1 = [1,2,3,4,5]
+accumulate_2 = itertools.accumulate(accumulate_1)
+print(type(accumulate_2))
+'''
+
+import random
+
+some_list = [1,2,3,4,5]
+some_choice = random.choice(some_list)
+
+print(some_choice)
