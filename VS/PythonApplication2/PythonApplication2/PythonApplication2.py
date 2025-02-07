9941e2e30fa57ff2fd008c600682575e53335624
Alexy Sviridov <unskillton@gmail.com>
2025-02-07 09:01:28 +0300

    itertools some

--- VS/PythonApplication2/PythonApplication2/PythonApplication2.py
+++ VS/PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -10,4 +10,23 @@ some_choice = random.choice(some_list)
 
 accumulate_sum = list(itertools.accumulate(some_list))
 
-print(accumulate_sum)
+#print(accumulate_sum)
+
+combinations_some = list(itertools.combinations(some_list, 2))
+combinations_some_with_replacement = list(itertools.combinations_with_replacement(some_list, 2))
+
+#print(*combinations_some)
+#print(*combinations_some_with_replacement)
+
+product_some = list(itertools.product('ABCD', 'xy'))
+
+#print(*product_some)
+
+starmap_some = list(itertools.starmap(pow, [(2,5), (3,2), (10,3)]))
+
+#print(*starmap_some)
+
+takewhile_some = list(itertools.takewhile(lambda x: x<5, [1,4,6,4,1]))
+
+#print(*takewhile_some)
+
