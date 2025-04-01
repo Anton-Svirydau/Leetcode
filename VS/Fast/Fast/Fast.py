7e8bbe703d05115486d344684f626cc57b3f5b8a
Alexy Sviridov <unskillton@gmail.com>
2025-04-01 08:29:26 +0300

    Инкапсуляция

--- VS/Fast/Fast/Fast.py
+++ VS/Fast/Fast/Fast.py
@@ -1,1 +1,21 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance  
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            return amount
+        return "No amount"
+
+    def get_balance(self):
+        return self.__balance 
+
+account = BankAccount(1000)
+print(account.get_balance())  # 1000
+# print(account.__balance)  # Error: AttributeError
+
 
