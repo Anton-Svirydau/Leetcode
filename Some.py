8fb1ae38c5567b9ca57a588bde3a926b927bc5ee
Alexy Sviridov <unskillton@gmail.com>
2025-07-07 13:09:19 +0300

    test

--- Some.py
+++ Some.py
@@ -13,5 +13,37 @@ def longest_common_prefix(strs):
 
     return prefix
 
-
 print(longest_common_prefix(["flower","flow","flight"]))
+
+def test_plus2():
+    prime_nums = []
+    for num in range(1, 50):
+        for div in range(2, num):
+            if num % div == 0:
+                break
+        else:
+            prime_nums.append(num)
+    assert plus2(prime_nums) == [3, 4, 5, 7, 9, 13, 15, 19, 21, 25, 31, 33, 39, 43, 45, 49]
+
+def test_multiply2():
+    prime_nums = []
+    for num in range(1, 50):
+        for div in range(2, num):
+            if num % div == 0:
+                break
+        else:
+            prime_nums.append(num)
+    assert multiply2(prime_nums) == [2, 4, 6, 10, 14, 22, 26, 34, 38, 46, 58, 62, 74, 82, 86, 94]
+
+def test_exponent2():
+    prime_nums = []
+    for num in range(1, 50):
+        for div in range(2, num):
+            if num % div == 0:
+                break
+        else:
+            prime_nums.append(num)
+    assert exponent2(prime_nums) == [1, 4, 9, 25, 49, 121, 169, 289, 361, 529, 841, 961, 1369, 1681, 1849, 2209]
+
+def sum2(x, y):
+    return x + y
---
+++ test.py
@@ -0,0 +1,7 @@
+
+from Some import sum2
+
+def test_sum2():
+    assert sum2(15, 8) == 23
+
+test_sum2()
